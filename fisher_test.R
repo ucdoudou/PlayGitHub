77a8c9340e29f1e10bd54429291e8f8c1c77823b
Wei Guo <guowei123@gmail.com>
2015-12-31 16:26:44 -0800

    Wei add new lines 1st

--- fisher_test.R
+++ fisher_test.R
@@ -6,3 +6,7 @@ fisher.test(sentiment)
 
 sentiment<-rbind(c(23,12,1),c(0,0,0),c(2,2,5))     
 fisher.test(sentiment)
+
+###Wei add new lines 1st
+sentiment<-rbind(c(23222,122112,1),c(0,0,0),c(2,2,5))     
+fisher.test(sentiment)
